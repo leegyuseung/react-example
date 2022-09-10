8b928e8307d14c287e2f4f12856a1a79cf384f95
leegyuseung <lgs4002@naver.com>
2022-09-10 20:22:12 +0900

    카드 컴포넌트 추가

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const AddUserHandler = (event) => {
@@ -6,13 +8,15 @@ const AddUser = (props) => {
   };
 
   return (
-    <form onSubmit={AddUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username" />{" "}
-      <label htmlFor="age">Age (Years)</label>
-      <input type="number" id="age" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card cssClass={classes.input}>
+      <form onSubmit={AddUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />{" "}
+        <label htmlFor="age">Age (Years)</label>
+        <input type="number" id="age" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
