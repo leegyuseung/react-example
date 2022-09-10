316121ea6e3cdd0b70fe5f2329280f3fe87ba62c
leegyuseung <lgs4002@naver.com>
2022-09-10 20:26:12 +0900

    button 컴포넌트 추가

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
@@ -14,7 +15,7 @@ const AddUser = (props) => {
         <input type="text" id="username" />{" "}
         <label htmlFor="age">Age (Years)</label>
         <input type="number" id="age" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
